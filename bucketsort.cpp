c6fa9210248c3677bdfef4e02ae3582929bc98c3
ajyxwDean <110725838+ajyxwDean@users.noreply.github.com>
2022-08-29 14:23:56 +0800

    Add files via upload

---
+++ bucketsort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void bucketSort(int data[], int len)
+{
+    //find minx max value of the array data
+    int min = data[0];
+    int max = min;
+    for(int i = 1; i < len; i++)
+    {
+        if(data[i] < min)
+            min = data[i];
+ 
+        if(data[i] > max)
+            max = data[i];
+    }
+ 
+    //get the bucket counts;
+    int bucketCounts = (max-min)/len + 1;
+ 
+    vector<vector<int>> bucketArrays;
+    for(int i = 0; i < bucketCounts; i++)
+    {
+        //position 0 used to keep the data count store in this bucket
+        vector<int> bucket;
+        bucketArrays.push_back(bucket);
+    }
+ 
+    //assign each value to bucket arrays.
+    for(int j = 0; j < len; j++)
+    {
+        int num = (data[j]-min)/len;
+        bucketArrays[num].push_back(data[j]);
+    }
+ 
+    //sort each bucket
+    for(int i = 0; i < bucketCounts; i++)
+    {
+        sort(bucketArrays[i].begin(),bucketArrays[i].end());
+    }
+ 
+    int index = 0;
+    //collect value from radix arrays to data
+    for(int k = 0; k < bucketCounts; k++)
+    {
+        for(int s = 0; s < bucketArrays[k].size(); s++)
+        {
+            data[index++] = bucketArrays[k][s];
+        }
+    }
+}
+ 
+int main() {
+ 
+    int arr[10] = {18,11,28,45,23,50};
+    bucketSort(arr, 6);
+ 
+    for(int i = 0; i < 6; i++)
+        cout << arr[i] << endl;
+ 
+    return 0;
+}
---
+++ testb622.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int i,j,k,n,m,f[10010],p1,p2,p3;
+int find(int k){
+    if(f[k]==k) return k;
+    return f[k]=find(f[k]);
+}
+int main(){
+    cin >> n >> m;
+    for(int i=1;i<=n;i++) f[i]=i;
+    for(int i=1;i<=m;i++){
+        cin>>p1>>p2>>p3;
+        if(p1==1){
+            f[find(p2)]=find(p3);
+        }
+        if(p1==2)
+            if(find(p2)==find(p3))
+                printf("Y\n");
+            else
+                printf("N\n");
+    }
+    return 0;
+}
